81d54d34eb9c7710604aca26ecf1c947d221e908
Edward Boland <mr.eboland@gmail.com>
2021-06-01 17:02:45 -0400

    Literals, variables, and wildcards in patterns

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,74 @@ fn main() {
 
 
 
+    // Literals, Variables, and Wildcards in Patterns
+
+    // We've shown match expressions working with enums earlier. Other types can be matched too. We can use match with an integer value. Integer literals like 0 and 1 can serve as patterns:
+    match meadow.count_rabbits() {
+        0 => {} // nothing to say
+        1 => println!("A rabbit is nosing around in the clover."),
+        n => println!("There are {} rabbits hopping about in the meadow", n)
+    }
+
+    // The pattern 0 matches if there are no rabbits in the meadow. 1 matches if there is just one. If there are two or more rabbits, we reach the third pattern, n. This pattern is just a var name. It can match any value, and the matched value is moved or copied into a new local var. So in this case, the value of meadow.count_rabbits() is stored in a new local variable n, which we then print.
+
+    // Other literals can be used as patterns too, including Booleans, characters, and even strings:
+    let calendar =
+        match settings.get_string("calendar") {
+            "gregorian" => Calendar::Gregorian,
+            "chinese" => Calendar::Chinese,
+            "ethiopian" => Calendar::Ethiopian,
+            other => return parse_error("calendar", other)
+        };
+
+    // In this example, other serves as a catch-all pattern, like n in the previous example. These patterns play the same role as a default case in a switch statement, matching values that don't match any of the other patterns.
+
+    // If we need a catch-all pattern, but we don't care about the matched value, we can use a single underscore _ as a pattern. The wildcard pattern:
+    let caption =
+        match photo.tagged_pet() {
+            Pet::Tyrannosaur => "RRRRAAAHHHHHHHHH",
+            Pet::Samoyed => "dog thoughts*",
+            _ => "I'm cute, love me" // generic caption, works for any pet
+        };
+
+    // The wildcard pattern matches any value, but without storing it anywhere. Since Rust requires every match expression to handle all possible values, a wildcard is often required at the end. Even if we're very sure the remaining cases can't occur, we must at least add a fallback arm that panics:
+        
+    // There are many Shapes, but we only support "selecting"
+    // either some text, or everything in a rectangular area.
+    // You can't select an ellipse or trapezoid.
+    match document.selection() {
+        Shape::TextSpan(start, end) => paint_text_selection(start, end),
+        Shape::Rectangle(rect) => paint_rect_selection(rect),
+        _ => panic!("unexpected selection type")
+    }
+
+    // It's worth noting that existing variables can't be used in patterns. Suppose we're implementing a board game with hexagonal spaces, and the player just clicked to move a piece. To confirm that the clock was valid, we might try something like this:
+    fn check_move(current_hex: Hex, click: Point) -> game::Result<Hex> {
+        match point_to_hex(click) {
+            None =>
+                Err("That's not a game space."),
+            Some(current_hex) => // try to match if user clicked the current_hex
+                // (it doesn't work, see explanation below)
+                Err("You are already there! You must click somewhere else."),
+            Some(other_hex) =>
+                Ok(other_hex)
+        }
+    }
+
+    // This fails because identifiers in patterns introduce new variables. The pattern Some(current_hex) here creates a new local variable current_hex, shadowing the argument current_hex. Rust emits several warnings about this code, in particular, the last arm of the match is unreachable. To fix it, use an if expression:
+    Some(hex) =>
+        if hex == current_hex {
+            Err("You are already there! You must click somewhere else")
+        } else {
+            Ok(hex)
+        }
+
+    // We'll cover guards, which offer another way to solve the above problem.
+
+
     
+    
+
 
 
 }
